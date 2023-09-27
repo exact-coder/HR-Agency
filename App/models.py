1f9411b603d334e57b9dc067f66eb26f07dbccd4
Programming Bees <hasanakash799@gmail.com>
2023-09-28 01:16:38 +0600

    chore: added email check in DB

--- App/models.py
+++ App/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Registered_email(models.Model):
+    email = models.CharField(_("Email"), max_length=50)
+
+    def __str__(self) -> str:
+        return self.email
--- App/views.py
+++ App/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
+from .models import Registered_email
 
 
 # Create your views here.
@@ -15,32 +16,44 @@ def opportunities(request):
 
 def email_frontend(request):
     if request.method == "POST":
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
-        experience = request.POST.get('experience')
-        skills = request.POST.get('skills')
-        
-        template = loader.get_template('resume_form.txt')
 
-        context = {'name':name,'age':age,'email':email,'phone':phone,'address':address,'experience':experience,'skills':skills}
+        email = request.POST['email']
+        if Registered_email.objects.filter(email=email).exists():
+            messages.error(request,"We already have your resume in our DB")
+            return HttpResponseRedirect('/opportunities')
+        else:
+            name = request.POST.get('name')
+            age = request.POST.get('age')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            address = request.POST.get('address')
+            experience = request.POST.get('experience')
+            skills = request.POST.get('skills')
 
-        message = template.render(context)
+            # Register inside DB
+            contact = Registered_email()
+            contact.email = email
+            contact.save()
+            # ===================
+            
+            template = loader.get_template('resume_form.txt')
 
-        """ email = EmailMultiAlternatives(
-            "Frontend - Candidate",message,
-            "Frontend Opportunity",['hasanakash799@gmail.com',email]
-        ) """
-        email = EmailMultiAlternatives(
-            "Frontend - Candidate",message,
-            "Frontend Opportunity",['hasanakash799@gmail.com']
-        )
-        email.content_subtype = "html"
-        file = request.FILES['file']
-        email.attach(file.name,file.read(),file.content_type)
-        email.send()
-        messages.success(request,"Successfully Send. Send a copy to your email also!!")
-        return HttpResponseRedirect("/")
+            context = {'name':name,'age':age,'email':email,'phone':phone,'address':address,'experience':experience,'skills':skills}
+
+            message = template.render(context)
+
+            """ email = EmailMultiAlternatives(
+                "Frontend - Candidate",message,
+                "Frontend Opportunity",['hasanakash799@gmail.com',email]
+            ) """
+            email = EmailMultiAlternatives(
+                "Frontend - Candidate",message,
+                "Frontend Opportunity",['hasanakash799@gmail.com']
+            )
+            email.content_subtype = "html"
+            file = request.FILES['file']
+            email.attach(file.name,file.read(),file.content_type)
+            email.send()
+            messages.success(request,"Successfully Send. Send a copy to your email also!!")
+            return HttpResponseRedirect("/")
     return HttpResponseRedirect("/opportunities")

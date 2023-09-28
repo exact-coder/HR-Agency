82d25ae1acc3a25922096965dc7deed01cb5b7fd
Programming Bees <hasanakash799@gmail.com>
2023-09-28 16:12:59 +0600

    chore: added custom admin panel

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Registered_email
 # Register your models here.
+
+@admin.register(Registered_email)
+class Registered_EmailAdmin(admin.ModelAdmin):
+    list_display = ['id','email']
+    list_display_links =['email']
+    search_fields = ['email']
+    list_per_page = 10
--- hr_agency/settings.py
+++ hr_agency/settings.py
@@ -30,12 +30,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # External Liberaries
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom app
     'App',
 ]
 
